421e52ecedcd2618808271983f2f0b83a47f5e65
Tatiana Sennikova <sennikovatatiana@gmail.com>
2018-06-09 22:50:39 +0200

    make responses json

--- login/login.py
+++ login/login.py
@@ -1,13 +1,13 @@
-from flask import Flask, request, make_response, abort
+from flask import Flask, request, jsonify
 from .models import db, User
-import re
-from jsonschema import validate, ValidationError
+from jsonschema import Draft4Validator
 
 USER_DATA_SCHEMA = {
     "type": "object",
     "properties": {
         "email": {
             "type": "string",
+            "pattern": '[\w._+]+[@]\w+[.]\w+',
             "minLength": 5,
             "maxLength": 120,
         },
@@ -17,8 +17,24 @@ USER_DATA_SCHEMA = {
             "maxLength": 120,
         },
     },
-    "required": ["email", "password"]
+    "required": ["email", "password"],
+    "additionalProperties": False
 }
+
+
+def get_response_with_json_and_status_code(message, status):
+    response = jsonify(message=message)
+    response.status_code = status
+    return response
+
+
+def get_errors_from_schema_validation_for_request(request_json, schema=USER_DATA_SCHEMA):
+    validator = Draft4Validator(schema)
+    errors = validator.iter_errors(request_json)
+    error_messages = [{"property": error.path.pop() if error.path else "",
+                       "message": error.message if error.validator != 'pattern' else "Incorrect format"}
+                      for error in errors]
+    return error_messages
 
 
 def create_app(db_path, secret_key):
@@ -37,74 +53,60 @@ def create_app(db_path, secret_key):
         if token:
             user = User.get_user_if_valid_auth_token(token, secret_key)
             if user:
-                return 'Welcome!', 200
-        abort(401, 'Please login as a valid user')
+                return get_response_with_json_and_status_code('Welcome!', 200)
+        return get_response_with_json_and_status_code('Please login as a valid user', 401)
 
     @app.route('/register', methods=['POST'])
     def register():
-        request_json = None
         try:
             request_json = request.json
-            validate(request_json, USER_DATA_SCHEMA)
-        except ValidationError as e:
-            abort(400, e.message)
+            errors = get_errors_from_schema_validation_for_request(request_json)
+            if errors:
+                return get_response_with_json_and_status_code(errors, 400)
         except:
-            abort(400, "Request body must be valid JSON")
+            return get_response_with_json_and_status_code("Request body must be valid JSON", 400)
 
         email = request_json['email']
-        if not is_valid_email(email):
-            abort(400, 'Email should be in correct format')
         password = request_json['password']
-
         user = User.query.filter_by(email=email).one_or_none()
-        if user:
-            if user.is_confirmed:
-                abort(400, "Email has been registered")
-            user.set_password(password)
-            db.session.commit()
-        else:
+        if user and user.is_confirmed:
+            return get_response_with_json_and_status_code("Email has been registered already", 400)
+        if not user:
             user = User(email=email, password=password)
             db.session.add(user)
             db.session.commit()
         send_confirmation_email(user)
-        return "Email confirmation link is sent", 201
-
-    def is_valid_email(email):
-        email_regex = re.compile('[\w._+]+[@]\w+[.]\w+', re.UNICODE)
-        return email_regex.match(email)
+        return get_response_with_json_and_status_code("Email confirmation link is sent", 201)
 
     def send_confirmation_email(user):
         confirmation_link = User.generate_email_token(user, secret_key)
-        app.logger.info(
-            'Confirm the email: {}'.format(request.url_root + 'confirm/' + confirmation_link))
+        app.logger.info(f"Confirm the email: {request.url_root}confirm/{confirmation_link}")
 
     @app.route('/confirm/<email_token>', methods=['GET'])
     def confirm_email(email_token):
         user = User.get_user_if_valid_email_token(email_token, secret_key)
         if not user:
-            abort(404, 'Confirmation link is invalid')
+            return get_response_with_json_and_status_code("Confirmation link is invalid", 404)
         user.is_confirmed = True
         db.session.commit()
-        return 'Email confirmed', 201
+        return get_response_with_json_and_status_code("Email is confirmed", 201)
 
     @app.route('/login', methods=['POST'])
     def login():
-        request_json = None
         try:
             request_json = request.json
-            validate(request_json, USER_DATA_SCHEMA)
-        except ValidationError as e:
-            abort(400, e.message)
+            errors = get_errors_from_schema_validation_for_request(request_json)
+            if errors:
+                return get_response_with_json_and_status_code(errors, 400)
         except:
-            abort(400, "Request body must be valid JSON")
+            return get_response_with_json_and_status_code("Request body must be valid JSON", 400)
 
         email = request_json['email']
         password = request_json['password']
-
         user = get_user_if_valid_login(email, password)
         if user:
             return get_response_with_auth_token(user)
-        abort(401, "Email and/or password are invalid")
+        return get_response_with_json_and_status_code("Email and/or password are invalid", 401)
 
     def get_user_if_valid_login(email, password):
         confirmed_user = User.query.filter_by(email=email, is_confirmed=True).one_or_none()
@@ -114,7 +116,7 @@ def create_app(db_path, secret_key):
 
     def get_response_with_auth_token(user):
         token = user.generate_auth_token(secret_key)
-        response = make_response()
+        response = jsonify(token=token.decode("utf-8"))
         response.set_cookie('token', token)
         return response
 
